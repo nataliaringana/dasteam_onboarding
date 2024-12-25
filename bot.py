3ab79bc0b772f32fc31516bc2ada6837e9f30f2b
Natalia Kiubaev <natalia.kiubaev@fourenergy.de>
2024-12-25 03:12:23 +0100

    fix secret leak

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
 
 # Функция для обработки команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -7,7 +8,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # Основная часть программы
 if __name__ == '__main__':
-    TOKEN = '7548766470:AAFD_UCqmoWygdgK3nGHsILLGAiXd55lq00'
+  
+    TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+#TOKEN = '7548766470:AAFD_UCqmoWygdgK3nGHsILLGAiXd55lq00'
 
     app = ApplicationBuilder().token(TOKEN).build()
 
