c3b602e823bdd802f7f5c2e929712b69254f7ff8
Natalia Kiubaev <natalia.kiubaev@fourenergy.de>
2024-12-25 09:40:27 +0100

    add webhook

--- bot.py
+++ bot.py
@@ -8,13 +8,22 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # Основная часть программы
 if __name__ == '__main__':
-  
+    # Токен бота
     TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-#TOKEN = '7548766470:AAFD_UCqmoWygdgK3nGHsILLGAiXd55lq00'
 
+    # URL вашего сервиса
+    WEBHOOK_URL = os.getenv('WEBHOOK_URL')  # например, "https://your-app-name.onrender.com"
+
+    # Создайте приложение бота
     app = ApplicationBuilder().token(TOKEN).build()
 
+    # Добавьте обработчик команды /start
     app.add_handler(CommandHandler('start', start))
 
-    print("Бот запущен. Нажмите Ctrl+C для остановки.")
-    app.run_polling()
+    # Установите Webhook
+    app.run_webhook(
+        listen="0.0.0.0",  # Слушаем все IP-адреса
+        port=int(os.getenv('PORT', 8443)),  # Используем порт из Render
+        url_path=TOKEN,
+        webhook_url=f"{WEBHOOK_URL}/{TOKEN}"
+    )
