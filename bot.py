7ea00a1d9711adea133dabf61b9260d14571cf24
nataliaringana <natalia.ringana23@gmail.com>
2025-04-03 15:24:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,20 @@
-import os
 import telebot
+import os
+from flask import Flask, request
 
-API_TOKEN = os.getenv("API_TOKEN")  # Token aus Umgebungsvariable laden
+API_TOKEN = os.getenv("API_TOKEN")
 bot = telebot.TeleBot(API_TOKEN)
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Hallo! Ich bin dein Telegram-Bot.")
+app = Flask(__name__)
 
-bot.polling()
+@app.route("/", methods=["POST"])
+def webhook():
+    json_str = request.get_data().decode("UTF-8")
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "OK", 200
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url="https://api.render.com/deploy/srv-cvn8id1r0fns738jm7l0?key=8roahJ-SX3w")
+    app.run(host="0.0.0.0", port=5000)
