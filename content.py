6fe9e4fc8be7c865dee900ee028fb6bf5bfe2212
nataliaringana <natalia.ringana23@gmail.com>
2025-04-05 01:34:26 +0200

    Update content.py

--- content.py
+++ content.py
@@ -1,6 +1,6 @@
 content = [
     {"type": "text", "data": "Hallo liebe/r {Vorname}! 😊\n\n🎉 HERZLICH WILLKOMMEN IM TEAM! 🎉\nIch freue mich, dich auf deinem Weg bei RINGANA zu begleiten. Gemeinsam schaffen wir Großes – mit Leichtigkeit und Freude"},
-    {"type": "video", "data": "https://drive.google.com/uc?id=1KlIj_WqsURqs7wKzuySAtfNZf1MzGSBe&export=download"},  # Ты можешь вставить своё видео
+    {"type": "video", "data": "https://www.dropbox.com/scl/fi/cx5nepzqawpjlkdkvur64/OnboardinVideo1.mp4?rlkey=2lh83vqn4m02ripifmwl77gd1&st=izai5ct9&dl=0"},  # Ты можешь вставить своё видео
     {"type": "text", "data": "👉 Was erwartet dich?\nAlles, was du für einen erfolgreichen Start benötigst, findest du hier Schritt für Schritt.\n\n💡 Bist du bereit?\nKlicke auf „Los geht’s“, und wir starten gemeinsam durch!"},
     {"type": "button", "data": "Los geht’s"}  # Кнопка для следующего шага
 ]
