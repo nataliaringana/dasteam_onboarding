a3bae358f47fdba71b12ea1fedef90e2acd4806f
nataliaringana <natalia.ringana23@gmail.com>
2025-04-04 22:47:54 +0200

    Create content.py

---
+++ content.py
@@ -0,0 +1,6 @@
+content = [
+    {"type": "text", "data": "Привет! Это первое сообщение."},
+    {"type": "video", "data": "https://example.com/sample.mp4"},
+    {"type": "text", "data": "А вот и третье сообщение!"},
+]
+v
