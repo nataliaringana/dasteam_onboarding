f8a9727fe8023aad30f7e35310702e95d1459091
nataliaringana <natalia.ringana23@gmail.com>
2025-04-05 02:12:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,127 +1,280 @@
-import telebot
-import time
+from flask import Flask, request, Response
+import requests
+import json
 
-# Создаем объект бота и токен
-bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
 
-# Словарь для хранения статистики чата
-stats = {}
+# token that we get from the BotFather
+TOKEN = "<Your Bot Token>"
 
-# Обработчик команды /start
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "Привет! Я бот для управления чатом. Напиши /help, чтобы узнать, что я умею.")
+app = Flask(__name__)
 
-# Обработчик команды /help
-@bot.message_handler(commands=['help'])
-def help(message):
-    bot.reply_to(message, "/kick - кикнуть пользователя\n/mute - замутить пользователя на определенное время\n/unmute - размутить пользователя\n/stats - показать статистику чата\n/selfstat - показать свою статистику")
+# Reading the JSON message when the user send any type of file to the bot and extracting the chat id of the user and the file id for the file that user send to the bot
+def tel_parse_get_message(message):
+    print("message-->", message)
 
-# Обработчик команды /kick
-@bot.message_handler(commands=['kick'])
-def kick_user(message):
-    if message.reply_to_message:
-        chat_id = message.chat.id
-        user_id = message.reply_to_message.from_user.id
-        user_status = bot.get_chat_member(chat_id, user_id).status
-        if user_status == 'administrator' or user_status == 'creator':
-            bot.reply_to(message, "Невозможно кикнуть администратора.")
-        else:
-            bot.kick_chat_member(chat_id, user_id)
-            bot.reply_to(message, f"Пользователь {message.reply_to_message.from_user.username} был кикнут.")
+    try:  # if the file is an image
+        g_chat_id = message['message']['chat']['id']
+        g_file_id = message['message']['photo'][0]['file_id']
+        print("g_chat_id-->", g_chat_id)
+        print("g_image_id-->", g_file_id)
+
+        return g_file_id
+    except:
+        try:  # if the file is a video
+            g_chat_id = message['message']['chat']['id']
+            g_file_id = message['message']['video']['file_id']
+            print("g_chat_id-->", g_chat_id)
+            print("g_video_id-->", g_file_id)
+
+            return g_file_id
+        except:
+            try:  # if the file is an audio
+                g_chat_id = message['message']['chat']['id']
+                g_file_id = message['message']['audio']['file_id']
+                print("g_chat_id-->", g_chat_id)
+                print("g_audio_id-->", g_file_id)
+
+                return g_file_id
+            except:
+                try:  # if the file is a document
+                    g_chat_id = message['message']['chat']['id']
+                    g_file_id = message['message']['document']['file_id']
+                    print("g_chat_id-->", g_chat_id)
+                    print("g_file_id-->", g_file_id)
+
+                    return g_file_id
+                except:
+                    print("NO file found found-->>")
+
+
+# Reading the JSON format when we send the text message and extracting the chat id of the user and the text that user send to the bot
+def tel_parse_message(message):
+    print("message-->", message)
+    try:
+        chat_id = message['message']['chat']['id']
+        txt = message['message']['text']
+        print("chat_id-->", chat_id)
+        print("txt-->", txt)
+
+        return chat_id, txt
+    except:
+        print("NO text found-->>")
+
+    try:
+        cha_id = message['callback_query']['from']['id']
+        i_txt = message['callback_query']['data']
+        print("cha_id-->", cha_id)
+        print("i_txt-->", i_txt)
+
+        return cha_id, i_txt
+    except:
+        pass
+
+
+# Get the Text message response from the bot
+def tel_send_message(chat_id, text):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    payload = {
+        'chat_id': chat_id,
+        'text': text
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the Image response from the bot by providing the image link
+def tel_send_image(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
+    payload = {
+        'chat_id': chat_id,
+        'photo': "https://raw.githubusercontent.com/fbsamples/original-coast-clothing/main/public/styles/male-work.jpg"
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the Poll response from the bot
+def tel_send_poll(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendPoll'
+    payload = {
+        'chat_id': chat_id,
+        "question": "In which direction does the sun rise?",
+        # options are provided in json format
+        "options": json.dumps(["North", "South", "East", "West"]),
+        "is_anonymous": False,
+        "type": "quiz",
+        # Here we are providing the index for the correct option(i.e. indexing starts from 0)
+        "correct_option_id": 2
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the Button response in the keyboard section
+def tel_send_button(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+    payload = {
+        'chat_id': chat_id,
+        'text': "What is this?",    # button should be in the propper format as described
+        'reply_markup': {
+            'keyboard': [[
+                {
+                    'text': 'supa'
+                },
+                {
+                    'text': 'mario'
+                }
+            ]]
+        }
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the Inline button response
+def tel_send_inlinebutton(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+    payload = {
+        'chat_id': chat_id,
+        'text': "What is this?",
+        'reply_markup': {
+            "inline_keyboard": [[
+                {
+                    "text": "A",
+                    "callback_data": "ic_A"
+                },
+                {
+                    "text": "B",
+                    "callback_data": "ic_B"
+                }]
+            ]
+        }
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the Button response from the bot with the redirected URL
+def tel_send_inlineurl(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+    payload = {
+        'chat_id': chat_id,
+        'text': "Which link would you like to visit?",
+        'reply_markup': {
+            "inline_keyboard": [
+                [
+                    {"text": "google", "url": "http://www.google.com/"},
+                    {"text": "youtube", "url": "http://www.youtube.com/"}
+                ]
+            ]
+        }
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the Audio response from the bot by providing the URL for the audio
+def tel_send_audio(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendAudio'
+
+    payload = {
+        'chat_id': chat_id,
+        "audio": "http://www.largesound.com/ashborytour/sound/brobob.mp3",
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the Document response from the bot by providing the URL for the Document
+def tel_send_document(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendDocument'
+
+    payload = {
+        'chat_id': chat_id,
+        "document": "http://www.africau.edu/images/default/sample.pdf",
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the Video response from the bot by providing the URL for the Video
+def tel_send_video(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendVideo'
+
+    payload = {
+        'chat_id': chat_id,
+        "video": "https://www.appsloveworld.com/wp-content/uploads/2018/10/640.mp4",
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+# Get the url for the file through the file id
+def tel_upload_file(file_id):
+    # Getting the url for the file
+    url = f'https://api.telegram.org/bot{TOKEN}/getFile?file_id={file_id}'
+    a = requests.post(url)
+    json_resp = json.loads(a.content)
+    print("json_resp-->", json_resp)
+    file_pathh = json_resp['result']['file_path']
+    print("file_pathh-->", file_pathh)
+
+    # saving the file to our computer
+    url_1 = f'https://api.telegram.org/file/bot{TOKEN}/{file_pathh}'
+    b = requests.get(url_1)
+    file_content = b.content
+    with open(file_pathh, "wb") as f:
+        f.write(file_content)
+
+
+# Reading the respomnse from the user and responding to it accordingly
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        msg = request.get_json()
+        try:
+            chat_id, txt = tel_parse_message(msg)
+            if txt == "hi":
+                tel_send_message(chat_id, "Hello, world!")
+            elif txt == "image":
+                tel_send_image(chat_id)
+            elif txt == "poll":
+                tel_send_poll(chat_id)
+            elif txt == "button":
+                tel_send_button(chat_id)
+            elif txt == "audio":
+                tel_send_audio(chat_id)
+            elif txt == "file":
+                tel_send_document(chat_id)
+            elif txt == "video":
+                tel_send_video(chat_id)
+            elif txt == "inline":
+                tel_send_inlinebutton(chat_id)
+            elif txt == "inlineurl":
+                tel_send_inlineurl(chat_id)
+            elif txt == "ic_A":
+                tel_send_message(chat_id, "You have clicked A")
+            elif txt == "ic_B":
+                tel_send_message(chat_id, "You have clicked B")
+
+            else:
+                tel_send_message(chat_id, 'from webhook')
+        except:
+            print("fromindex-->")
+
+        try:
+            file_id = tel_parse_get_message(msg)
+            tel_upload_file(file_id)
+        except:
+            print("No file from index-->")
+
+        return Response('ok', status=200)
     else:
-        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите кикнуть.")
-
-# Обработчик команды /mute
-@bot.message_handler(commands=['mute'])
-def mute_user(message):
-    if message.reply_to_message:
-        chat_id = message.chat.id
-        user_id = message.reply_to_message.from_user.id
-        user_status = bot.get_chat_member(chat_id, user_id).status
-        if user_status == 'administrator' or user_status == 'creator':
-            bot.reply_to(message, "Невозможно замутить администратора.")
-        else:
-            duration = 60 # Значение по умолчанию - 1 минута
-            args = message.text.split()[1:]
-            if args:
-                try:
-                    duration = int(args[0])
-                except ValueError:
-                    bot.reply_to(message, "Неправильный формат времени.")
-                    return
-                if duration < 1:
-                    bot.reply_to(message, "Время должно быть положительным числом.")
-                    return
-                if duration > 1440:
-                    bot.reply_to(message, "Максимальное время - 1 день.")
-                    return
-            bot.restrict_chat_member(chat_id, user_id, until_date=time.time()+duration*60)
-            bot.reply_to(message, f"Пользователь {message.reply_to_message.from_user.username} замучен на {duration} минут.")
-    else:
-        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите замутить.")
-
-# Обработчик команды /unmute
-@bot.message_handler(commands=['unmute'])
-def unmute_user(message):
-    if message.reply_to_message:
-        chat_id = message.chat.id
-        user_id = message.reply_to_message.from_user.id
-        bot.restrict_chat_member(chat_id, user_id, can_send_messages=True, can_send_media_messages=True, can_send_other_messages=True, can_add_web_page_previews=True)
-        bot.reply_to(message, f"Пользователь {message.reply_to_message.from_user.username} размучен.")
-    else:
-        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите размутить.")
-
-# Обработчик команды /stats
-@bot.message_handler(commands=['stats'])
-def chat_stats(message):
-    chat_id = message.chat.id
-    if chat_id not in stats:
-        bot.reply_to(message, "Статистика чата пуста.")
-    else:
-        total_messages = stats[chat_id]['total_messages']
-        unique_users = len(stats[chat_id]['users'])
-        bot.reply_to(message, f"Статистика чата:\nВсего сообщений: {total_messages}\nУникальных пользователей: {unique_users}")
-
-# Обработчик команды /selfstat
-@bot.message_handler(commands=['selfstat'])
-def user_stats(message):
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    username = message.from_user.username
-    if chat_id not in stats:
-        bot.reply_to(message, "Статистика чата пуста.")
-    else:
-        if user_id not in stats[chat_id]['users']:
-            bot.reply_to(message, "Вы еще не отправляли сообщений в этом чате.")
-        else:
-            user_messages = stats[chat_id]['users'][user_id]['messages']
-            total_messages = stats[chat_id]['total_messages']
-            percentage = round(user_messages / total_messages * 100, 2)
-            bot.reply_to(message, f"Статистика для пользователя @{username}:\nВсего сообщений: {user_messages}\nПроцент от общего количества сообщений: {percentage}%")
-
-bad_words = ['анкета', 'ссылка', 'уникальное предложение']
-
-# функция для проверки наличия запрещенных слов в сообщении
-def check_message(message):
-    for word in bad_words:
-        if word in message.text.lower():
-            return True
-    return False
-
-# обработчик сообщений
-@bot.message_handler(func=lambda message: True)
-def handle_message(message):
-    # проверяем сообщение на наличие запрещенных слов
-    if check_message(message):
-        # если есть хотя бы одно запрещенное слово, кикаем пользователя
-        bot.kick_chat_member(message.chat.id, message.from_user.id)
-        bot.send_message(message.chat.id, f"Пользователь {message.from_user.username} был удален из чата за использование запрещенных слов")
-    else:
-        # если запрещенных слов нет, обрабатываем сообщение дальше
-        print(message.text)
+        return "<h1>Welcome!</h1>"
 
 
-
-# Запускаем бота
-bot.infinity_polling(none_stop=True)
+if __name__ == '__main__':
+    app.run(threaded=True)
